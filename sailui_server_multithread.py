c07fe72e14858fbf948d74c841d1d14c65524e72
HadrienLG <hadrien.lg@wanadoo.fr>
2021-03-16 11:11:24 +0100

    Modification de la librairie ICM20948 depuis celle fournie par WaveShare

--- sailui_server_multithread.py
+++ sailui_server_multithread.py
@@ -83,21 +83,12 @@ def thread_capteurs(threadname, gyro, baro, therm):
     
     while True:
         # Gyroscope
-        gyro.icm20948_Gyro_Accel_Read()
-        gyro.icm20948MagRead()
-        gyro.icm20948CalAvgValue()
-        time.sleep(0.1)
-        gyro.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175,MotionVal[2] * 0.0175,
-                    MotionVal[3],MotionVal[4],MotionVal[5], 
-                    MotionVal[6], MotionVal[7], MotionVal[8])
-        pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
-        roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
-        yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
-        print("\r\n /-------------------------------------------------------------/ \r\n")
-        print('\r\n Roll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
-        print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))  
-        print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
-        print('\r\nMagnetic:      X = %d , Y = %d , Z = %d'%((Mag[0]),Mag[1],Mag[2]))
+        gyro.icm20948update()
+        print("-------------------------------------------------------------")
+        print('Roll = {:.2f}, Pitch = {:.2f}, Yaw = {:.2f}'.format(icm20948.Roll, icm20948.Pitch, icm20948.Yaw))
+        print('Acceleration:  X = {}, Y = {}, Z = {}'.format(icm20948.Acceleration[0], icm20948.Acceleration[1], icm20948.Acceleration[2]))  
+        print('Gyroscope:     X = {}, Y = {}, Z = {}'.format(icm20948.Gyroscope[0], icm20948.Gyroscope[1], icm20948.Gyroscope[2]))
+        print('Magnetic:      X = {}, Y = {}, Z = {}'.format(icm20948.Magnetic[0], icm20948.Magnetic[1], icm20948.Magnetic[2]))
         
         # Baromètre
         baro.LPS22HB_START_ONESHOT()
@@ -110,10 +101,12 @@ def thread_capteurs(threadname, gyro, baro, therm):
             u8Buf[0]=baro._read_byte(LPS_TEMP_OUT_L)
             u8Buf[1]=baro._read_byte(LPS_TEMP_OUT_H)
             TEMP_DATA=((u8Buf[1]<<8)+u8Buf[0])/100.0
+        print("-------------------------------------------------------------")
         print('Pressure = %6.2f hPa , Temperature = %6.2f °C\r\n'%(PRESS_DATA,TEMP_DATA))
 
         # Thermomètre
         temperature, humidite = therm.measurements
+        print("-------------------------------------------------------------")
         print('Temperature = {%6.2}f°C , Humidity = {%6.2}f%%'.format(temperature, humidite))
 
 def thread_serial(threadname, cidb, sss, status_led):
