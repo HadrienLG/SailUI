83d9fad3150297d86c3a79204518266048dd9a5c
HadrienLG <hadrien.lg@wanadoo.fr>
2021-06-18 00:01:08 +0200

    Simplification du programme : SERAIL -> MQTT, TODO: superviser le service, gérer les LEDs d'état et l'envoi à InfluxDB dans d'autres services

--- server/sailui_publish_gps.py
+++ server/sailui_publish_gps.py
@@ -27,40 +27,32 @@ SOFTWARE.
 
 # Configuration
 import config.hardware as hardware
+import config.mqttbroker as mqttbroker
 
 # Generic/Built-in
 import datetime
 import serial
 import time
 import os
-import socket
 import sys
 import threading
 from subprocess import check_call
 import logging
-import paho.mqtt.client as mqtt
 import pynmea2
 
-# Sensors
-from libs.LEDplus import LEDplus
-from gpiozero import Button, LED
+# Custom
 
-# Project modules
-from database import DataBase
 
 # Owned
 __author__ = "HadrienLG"
 __copyright__ = "Copyright 2021, SailUI"
 __credits__ = ["HadrienLG", "ChrisBiau",]
 __license__ = "MIT"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __maintainer__ = "HadrienLG"
 __email__ = "hadrien.lg@wanadoo.fr"
 __status__ = "OnGoing"
-__influence__ = {'InfluxDB':'https://www.influxdata.com/blog/getting-started-python-influxdb/',
-                 'Socket_Thread':'https://www.positronx.io/create-socket-server-with-multiple-clients-in-python/',
-                 'Server_Client':'https://stackoverflow.com/questions/43513337/multiclient-server-in-python-how-to-broadcast',
-                 'NMEA parsing':'https://github.com/Knio/pynmea2',
+__influence__ = {'NMEA parsing':'https://github.com/Knio/pynmea2',
                  'Waveshare Sense HAT': 'https://www.waveshare.com/wiki/Sense_HAT_(B)',
                  'MQTT': 'https://pypi.org/project/paho-mqtt/'}
 
@@ -68,92 +60,121 @@ __influence__ = {'InfluxDB':'https://www.influxdata.com/blog/getting-started-pyt
 logging.basicConfig(filename='sailui_publish_gps.log', level=logging.DEBUG)
 logging.info('Démarrage de SailUI-Publish GPS, début de journalisation')
 
+import paho.mqtt.client as mqtt
 
-def thread_serial(threadstop, db_pos, mqttclient, status_led):
-    identifiants = ['GGA', 'GLL', 'GSV', 'GSA', 'VTG', 'RMC']
+class MQTT():
+    def __init__(self, adresse='127.0.0.1', port=1883, debug=False):
+        # Création du client
+        self.client = mqtt.Client()
+        self.debug = debug
+        
+        # Définition des actions par défaut
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_publish = self.on_publish
+        
+        # Connexion
+        self.client.connect(adresse, port, 60)
+        self.client.loop_start()
     
+    # [MQTT] The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print('Connected to MQTT broker successfully!')
+        else:
+            print("Connected with result code " + mqtt.connack_string(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe("$SYS/#")
+
+    # [MQTT] The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        if self.debug:
+            print(msg.topic+" : "+str(msg.payload.decode()))
+        pass
+
+    # [MQTT] The callback for when a PUBLISH message is send to the server.
+    def on_publish(self, mosq, obj, mid):
+        if self.debug:
+            print("Publish mid: " + str(mid))
+        pass
+    
+    # [MQTT] Publish
+    def publish(self, sujet, message):
+        return self.client.publish(sujet, message)
+
+def nmea2payload(nmea):
+    '''
+    Mise en forme de la phrase NMEA vers une payload
+    '''
+    identifier = nmea.identifier()[2:]
+    
+    # Cas général
+    msgs = []
+    for champ, valeur in dict(zip(nmea.fields,nmea.data)).items():
+            msgs.append({'topic':'gps/'+identifier+'/'+champ[1].strip().replace(' ','_'),
+                         'payload':valeur})
+    
+    # Cas particulier
+    if identifier == 'RMC':
+        msgs.append({'topic':"gps/info/latitude", 'payload':nmea.latitude})
+        msgs.append({'topic':"gps/info/longitude", 'payload':nmea.longitude})
+        msgs.append({'topic':"gps/info/cap_vrai", 'payload':nmea.data[7]})
+    
+    if identifier == 'VTG':
+        msgs.append({'topic':"gps/info/vitesse", 'payload':nmea.data[4]}) # 'Speed over ground knots'
+        msgs.append({'topic':"gps/info/cap", 'payload':nmea.data[0]}) # 'True Track made good'
+
+    if identifier == 'GGA':
+        msgs.append({'topic':"gps/info/date", 'payload':nmea.data[0]}) # 'TimeStamp'
+        msgs.append({'topic':"gps/info/sats", 'payload':nmea.data[6]}) # 'Number of Satellites in use'
+        msgs.append({'topic':"gps/info/altitude", 'payload':nmea.data[8]})# 'Antenna Alt above sea level (mean)'
+
+    if identifier == 'GLL':
+        pass
+    if identifier == 'GSA':
+        pass
+    if identifier == 'GSV':
+        pass
+    
+    return msgs # payload
+
+
+def thread_serial(var, serialPort, mqttclient):
+    """
+    Gestion des sorties du module ublox Max-M8Q
+    Export vers le broker MQTT
+    """
+    identifiants = ['GGA', 'GLL', 'GSA', 'GSV', 'VTG', 'RMC']
     while True:
         try:
             # Lecture d'une ligne et décodage
             ligne = serialPort.readline()
             phrase = ligne.decode("utf-8")
-            logging.debug('[SERIAL] {}'.format(phrase))
+            logging.debug(f'[UBLOX-M8Q] {phrase}')
             
-            # Envoi à la base de données de la phrase NMEA brute
-            db_pos.add_point(phrase)
-
             # Si la phrase NMEA nous intéresse, on la partage aux clients
             id_nmea = phrase[3:6]
             if id_nmea in identifiants:
-                topic = f"gps/{id_nmea}"
-                result = mqttclient.publish(topic,phrase) # result: [code, message_id]
+                # Envoi du brut vers le broker MQTT
+                result = mqttclient.publish(f"gps/{id_nmea}/raw",phrase) # result: [code, message_id]
                 if result[0] != 0:
                     logging.exception(f"Echec de l'envoi du message NMEA {id_nmea} au broker")
-                if id_nmea == 'RMC':
-                    nmeaRMC = pynmea2.parse(phrase)
-                    msgs = [{'topic':"gps/info/latitude", 'payload':nmeaRMC.latitude},
-                            {'topic':"gps/info/longitude", 'payload':nmeaRMC.longitude},
-                            {'topic':"gps/info/cap_vrai", 'payload':nmeaRMC[7]},]
-                    for msg in msgs:
-                        result = mqttclient.publish(msg['topic'],msg['payload'])
-                        if result[0] != 0:
-                            logging.exception(f"Echec de l'envoi du message NMEA RMC au broker: {msg}")
-                if id_nmea == 'VTG':
-                    nmeaVTG = pynmea2.parse(phrase)
-                    msgs = [{'topic':"gps/info/vitesse", 'payload':nmeaVTG.data[4]}, # 'Speed over ground knots'
-                            {'topic':"gps/info/cap", 'payload':nmeaVTG.data[0]}, # 'True Track made good'
-                            ]
-                    for msg in msgs:
-                        result = mqttclient.publish(msg['topic'],msg['payload'])
-                        if result[0] != 0:
-                            logging.exception(f"Echec de l'envoi du message NMEA VTG au broker: {msg}")
-                if id_nmea == 'GGA':
-                    nmeaGGA = pynmea2.parse(phrase)
-                    msgs = [{'topic':"gps/info/date", 'payload':nmeaGGA.data[0]}, # 'TimeStamp'
-                            {'topic':"gps/info/sats", 'payload':nmeaGGA.data[6]}, # 'Number of Satellites in use'
-                            {'topic':"gps/info/altitude", 'payload':nmeaGGA.data[8]}, # 'Antenna Alt above sea level (mean)'
-                            ]
-                    for msg in msgs:
-                        result = mqttclient.publish(msg['topic'],msg['payload'])
-                        if result[0] != 0:
-                            logging.exception(f"Echec de l'envoi du message NMEA GGA au broker: {msg}")
+                    
+                # Extrait des phrases NMEA
+                nmea = pynmea2.parse(phrase)
+                
+                # Envoi des détails au broker MQTT
+                msgs = nmea2payload(nmea) # Préparation de la payload
+                for msg in msgs:
+                    result = mqttclient.publish(msg['topic'],msg['payload'])
+                    if result[0] != 0:
+                        logging.exception(f"Echec de l'envoi du message NMEA {id_nmea} au broker: {msg}")
+        
         # Gestion des erreurs
         except(UnicodeError):
             logging.exception('ThreadSerial, erreur Unicode: ',ligne)
-        if not threadstop():
-            logging.info('Arrêt du thread Serial')
-            print('Arrêt du thread Serial',threadstop())
-            break
-            
-def killsignal():
-    logging.info('Lancement du kill signal')
-    user_signal = False
-
-def shutdown():
-    logging.info('Extinction demandée')
-    check_call(['sudo', 'poweroff'])
-    
-# [MQTT] The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        logging.info('Connected to MQTT broker successfully!')
-    else:
-        logging.warning("Connected with result code " + mqtt.connack_string(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
-
-# [MQTT] The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
-    pass
-
-# [MQTT] The callback for when a PUBLISH message is send to the server.
-def on_publish(mosq, obj, mid):
-    #print("Publish mid: " + str(mid))
-    pass
-
 
 if __name__ == '__main__':
     logging.debug('Initialisation du programme')
@@ -161,72 +182,39 @@ if __name__ == '__main__':
     ##############################################
     # 0. Initialisation des différents composants
     ##############################################
-    # - GPIOs
-    # - port série
-    # - mqtt client
-    
-    user_signal = True
-     
-    # Initialisation de la led jaune de status, allumage 1s puis extinction
-    yellow1 = LEDplus(hardware.leds['yellow1'])
-    yellow1.on()
-    time.sleep(1)
-    yellow1.off()
-    logging.info(f'LED jaune1 démarrée')
-    
+   
     # Initialisation du port série
     port = "/dev/serial0"
     serialPort = serial.Serial(port, baudrate = 9600, timeout = 0.5)
     logging.info(f'Série, port {port} connecté')
-
-    # Connection à la base InfluxDB
-    InfluxPosition = DataBase('position')
-    logging.info(f'InfluxDB, connexions au serveur établie')
     
     # Client MQTT
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_publish = on_publish
-    client.connect("127.0.0.1", 1883, 60)
-    client.loop_start()
+    client = MQTT() # adresse=mqttbroker['adresse'], port=mqttbroker['port'])
+    client.publish({'topic':"gps/server_status/datetime", 'payload':datetime.datetime.now().strftime('%H:%M:%S-%d/%m/%y')})
+    client.publish({'topic':"gps/server_status/state", 'payload':'up'})
     logging.info(f'MQTT, connexions au serveur établie')
     
     ##############################################
     # 1. Lancement des threads
     ##############################################
-    # - écoute du port série/GPS et envoi à la BDD
-    
-    stop_thread = lambda : user_signal
     
     # Ecoute du port série
-    threadSerial_name = "Lecture du port série"
-    threadSerial = threading.Thread( target=thread_serial,
-                                     args=(stop_thread, InfluxPosition, client, yellow1) )
-    threadSerial.setName( threadSerial_name )
+    threadSerial = threading.Thread( target=thread_serial, args=(0, serialPort, client), name="Lecture du port série" )
     threadSerial.start()
-    logging.info(f'Thread {threadSerial_name} démarré')
-    yellow1.blink(0.5)
+    logging.info(f'Thread {threadSerial.getName()} démarré')
         
     ##############################################
     # 2. Supervision des threads
-    ##############################################
-    # - Boucle permanente pour superviser les threads
-    
-    while True and user_signal:
+    ##############################################    
+    while True:
         time.sleep(1) # Toutes les secondes
         try:
             if not threadSerial.is_alive():
                 message = '[{}] {} {}'.format(time.ctime(), threadSerial.getName(), threadSerial.is_alive())
+                client.publish({'topic':"gps/server_status/datetime", 'payload':datetime.datetime.now().strftime('%H:%M:%S-%d/%m/%y')})
+                client.publish({'topic':"gps/server_status/state", 'payload':'down'})
                 logging.warning(message)
-        except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-            user_signal = False
-            InfluxPosition.close()
-            yellow1.off()
-            logging.info("Done.\nExiting.")
+        except:
+            logging.exception('ThreadSerial is down...')
     else:
-        print('Kill signal has been pressed...')
-        killsignal()
-        InfluxPosition.close()
         logging.info('Going to shut down now...')
-        # shutdown()
